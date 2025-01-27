7281c24afa41830eb16c40cfe26aa4811ba22688
Nathan260300 <nathan162318@gmail.com>
2025-01-27 19:55:47 +0100

    Update start.py

--- start.py
+++ start.py
@@ -9,11 +9,8 @@ token = os.getenv("DISCORD_TOKEN")
 
 class MonBot(commands.Bot):
     async def setup_hook(self):
-        try:
-            await self.load_extension("cogs.games")
-            print("Extension 'games' chargée avec succès")
-        except Exception as e:
-            print(f"Erreur lors du chargement de l'extension 'games' : {e}")
+        for extension in ['games', 'moderation']:
+            await self.load_extension(f"cogs.{extension}")
 
 intents = discord.Intents.all()
 bot = MonBot(command_prefix="!", intents=intents)
@@ -28,4 +25,4 @@ async def on_ready():
         print(e)
 
 keep_alive()
-bot.run(token)
+bot.run(token=token)
