fea681ce9e1286f74114406d656119f116c9c32f
Nathan260300 <nathan162318@gmail.com>
2025-01-27 18:55:59 +0100

    Update start.py

--- start.py
+++ start.py
@@ -4,15 +4,25 @@ import discord
 from discord.ext import commands
 from keep_alive import keep_alive
 
-# Charger le token depuis le fichier .env
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
+class MonBot(commands.Bot):
+    async def setup_hook(self):
+        for extension in ['games', 'moderation']:
+            await self.load_extension(f"cogs.{extension}")
+
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = MonBot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"Connecté en tant que {bot.user}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"{len(synced)} commande(s) synchronisée(s)")
+    except Exception as e:
+        print(e)
 
+keep_alive()
 bot.run(token)
