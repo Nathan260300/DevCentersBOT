2b86eacf9ac02902652fefe9c9fad99f140c7d62
Nathan260300 <nathan162318@gmail.com>
2025-02-17 14:32:39 +0100

    Update games.py

--- cogs/games.py
+++ cogs/games.py
@@ -5,31 +5,39 @@ class GamesCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(help="Lance un dé à 6 faces et affiche le résultat.")
     async def dé(self, ctx):
         result = random.randint(1, 6)
         await ctx.send(f"**{ctx.author.name} lance un dé et obtient : __{result}__**")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(help="Effectue un tirage de pile ou face et affiche le résultat.")
     async def pileouface(self, ctx):
         result = random.choice(["pile", "face"])
         await ctx.send(f"**Résultat obtenu : __{result}__**")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(help="Effectue l'addition de deux nombres et affiche le résultat.")
     async def addition(self, ctx: commands.Context, a: int, b: int):
         await ctx.send(f"**{a} + {b} = __{a + b}__**")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(help="Effectue la soustraction de deux nombres et affiche le résultat.")
     async def soustraire(self, ctx: commands.Context, a: int, b: int):
         await ctx.send(f"**{a} - {b} = __{a - b}__**")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(help="Une blague...")
     async def ping(self, ctx):
         await ctx.send("**Pong 🏓 !**")
 
-    @commands.hybrid_command()
+    @commands.hybrid_command(help="Effectue la multiplication de deux nombres et affiche le résultat.")
     async def multiplication(self, ctx: commands.Context, a: int, b: int):
         await ctx.send(f"**{a} x {b} = __{a * b}__**")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+      if message.author == self.bot.user:
+        return
+      if self.bot.user.mentioned_in(message):
+        await message.reply("**Quoi 😠 ???!!!??**", mention_author=True)
+      await self.bot.process_commands(message)
 
 async def setup(bot):
     await bot.add_cog(GamesCog(bot))
