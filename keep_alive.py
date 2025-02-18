d7ec708dfa4d48dc5de9ea83036c447cd0616527
Nathan260300 <nathan162318@gmail.com>
2025-02-18 13:51:39 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from threading import Thread
 import os
 import sys
@@ -11,8 +11,12 @@ def home():
 
 @app.route("/restart", methods=["POST"])
 def restart():
-    
+   
+    token = request.headers.get("Authorization")
+    if token != "YUSEBHSEBHSEBHSEBHSEBHSEBHSEBHVGN14561231": 
+        return jsonify({"message": "Non autorisé"}), 403
 
+    
     os.execv(sys.executable, ['python'] + sys.argv)  
     
     return jsonify({"message": "Bot redémarre maintenant."}), 200
