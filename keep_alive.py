e2e68ee9c603621db6e5f57f47cfe2397e9ab8fe
Nathan260300 <nathan162318@gmail.com>
2025-02-18 13:58:27 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,26 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from threading import Thread
-import os
-import sys
 
 app = Flask("")
 
 @app.route("/")
 def home():
     return "Le bot est en ligne !"
-
-@app.route("/restart", methods=["POST"])
-def restart():
-   
-    token = request.headers.get("Authorization")
-    if token != "YUSEBHSEBHSEBHSEBHSEBHSEBHSEBHVGN14561231": 
-        return jsonify({"message": "Non autorisé"}), 403
-
     
-    os.execv(sys.executable, ['python'] + sys.argv)  
-    
-    return jsonify({"message": "Bot redémarre maintenant."}), 200
-
 def run():
     app.run(host="0.0.0.0", port=8080)
 
