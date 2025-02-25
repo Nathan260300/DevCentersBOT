3fc6cc2d9d9b8b74d00ec2fcc55716abefcc1112
Nathan260300 <nathan162318@gmail.com>
2025-02-25 13:52:47 +0100

    Update start.py

--- start.py
+++ start.py
@@ -23,7 +23,7 @@ async def on_ready():
         print(f"{len(synced)} commande(s) synchronisée(s)")
     except Exception as e:
         print(e)
-    activity = discord.Streaming(name="GitHub.", url="https://twitch.tv/rien")
+    activity = discord.Streaming(name="Github.", url="https://twitch.tv/rien")
     await bot.change_presence(status=discord.Status.online, activity=activity)
     print('Statut mis à jour')
 
