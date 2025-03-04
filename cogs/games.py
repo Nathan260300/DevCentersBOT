0416c92239113034344bfffb9facb145e0e3babc
Nathan260300 <nathan162318@gmail.com>
2025-03-04 19:59:49 +0100

    Update games.py

--- cogs/games.py
+++ cogs/games.py
@@ -69,12 +69,12 @@ class GamesCog(commands.Cog):
             await message.reply(embed=embed, mention_author=True)
         await self.bot.process_commands(message)
         
-        @commands.hybrid_command(help="Une blague...")
-        async def ping(self, ctx):
-          embed = discord.Embed(
-            title="Blague 🤪",
-            description="**Pong 🏓 !**",
-            color=discord.Color.purple()
+    @commands.hybrid_command(help="Une blague...")
+    async def ping(self, ctx):
+      embed = discord.Embed(
+        title="Blague 🤪",
+        description="**Pong 🏓 !**",
+        color=discord.Color.purple()
         )
         await ctx.send(embed=embed)
 
